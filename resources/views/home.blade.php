62dab0ed07f42ea8f4e322db2a35695a6957bf93
pa3lo <nardellipv@gmail.com>
2018-02-19 14:47:02 -0300

    adding theme

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.dashboard.dashboard')
 
 @section('content')
 <div class="container">
---
+++ resources/views/layouts/menu/footer.blade.php
@@ -0,0 +1,4 @@
+<footer class="main-footer">
+    <!-- Default to the left -->
+    <strong>Copyright &copy; 2017 <a href="#">GKinder</a>.</strong> 
+</footer>
